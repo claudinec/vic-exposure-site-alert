fb096aca6300b3a61a3518dea8e30fb3deaa7632
Claudine Chionh <github@claudinec.net>
2021-08-29 17:40:06 +1000

    Move all code to one file

--- vic_exposure_site_alert.py
+++ vic_exposure_site_alert.py
@@ -1,9 +1,14 @@
-# Parse exposure site data.
+# Periodically fetch Victorian Covid-19 exposure site data and alert via Pushcut.
 
+import csv
 import json
-import re
-from datetime import datetime, timedelta
+from datetime import datetime
 import requests
+
+data_csv_file = 'data/exposure-sites-data.csv'
+data_json_file = 'data/exposure-sites-data.json'
+data_fetched_str = {"data_fetched": datetime.now().isoformat()}
+data_fetched_file = 'data/data_fetched.json'
 
 suburbs_json_file = 'config/suburbs.json'
 pushcut_json_file = 'config/pushcut.json'
@@ -13,6 +18,28 @@ tier_match = '(Tier\s[0-9])'
 last_run_dt = datetime.now()
 last_run_str = {"last_run": last_run_dt.isoformat()}
 last_run_file = 'data/last_run.json'
+
+# Fetch the data to be parsed by data_parser.
+
+r = requests.get('https://drive.google.com/uc?export=download&id=1hULHQeuuMQwndvKy1_ScqObgX0NRUv1A', stream=True)
+
+if (r.ok):
+    data_json = []
+    data_str = r.content.decode()
+    with open(data_csv_file, mode='w',newline='\r') as csv_file_writer:
+        print(data_str, file=csv_file_writer)
+    with open(data_csv_file, mode='r',newline='\r') as csv_file_reader:
+        csv_reader = csv.DictReader(csv_file_reader)
+        for row in csv_reader:
+            data_json.append(row)
+    with open(data_json_file, mode='w') as json_file_writer:
+        json.dump(data_json, json_file_writer)
+    with open(data_fetched_file, mode='w') as data_fetched_file_writer:
+        json.dump(data_fetched_str, data_fetched_file_writer)
+else:
+    print(r.status_code)
+
+# Parse exposure site data.
 
 with open(suburbs_json_file, mode='r') as suburbs_file_reader:
     suburbs_list = json.load(suburbs_file_reader)
